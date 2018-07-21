3b5a8f28077cbc769470940e459a65b7cad7b4e9
d4g33z <jeff@mu.local>
2018-07-21 03:57:04 -0400

    Move install variables to top.

--- sysroot.sh
+++ sysroot.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+KERNEL_WORK=/usr/src/rpi_kernel
+CFLAGS="-O2 -pipe -march=armv7-a -mtune=cortex-a53 -mfpu=neon-vfpv4 -mfloat-abi=hard"
+CTARGET=armv7a-hardfloat-linux-gnueabi
+#SDCARD_DEV=mmcblk0p
+SDCARD_DEV=sdb
+SDCARD=/dev/${SDCARD_DEV}
+STAGE_BALL=${HOME}/Downloads/stage3-latest.tar.xz
+STAGE_URL="http://ftp.osuosl.org/pub/funtoo/funtoo-current/arm-32bit/raspi3/stage3-latest.tar.xz"
+SYSROOT=/home/sysroots/${CTARGET}
+RPI_KERN_BRANCH=rpi-4.9.y
+DISTCC_REMOTE_JOBS=21
+DISTCC_REMOTE_HOSTS="10.0.0.1,cpp,lzo"
+
 prompt_input_yN()
 {
     printf "$1? [y|N] " ; shift
@@ -49,18 +62,6 @@ sysroot_install()
         printf "error: must run as root\n"
         return 1;
     fi
-    DISTCC_REMOTE_JOBS=21
-    DISTCC_REMOTE_HOSTS="10.0.0.1,cpp,lzo" #local network
-    KERNEL_WORK=/usr/src/rpi_kernel
-    CFLAGS="-O2 -pipe -march=armv7-a -mtune=cortex-a53 -mfpu=neon-vfpv4 -mfloat-abi=hard"
-    CTARGET=armv7a-hardfloat-linux-gnueabi
-    #SDCARD_DEV=mmcblk0p
-    SDCARD_DEV=sdb
-    SDCARD=/dev/${SDCARD_DEV}
-    STAGE_BALL=${HOME}/Downloads/stage3-latest.tar.xz
-    STAGE_URL="http://ftp.osuosl.org/pub/funtoo/funtoo-current/arm-32bit/raspi3/stage3-latest.tar.xz"
-    SYSROOT=/home/sysroots/${CTARGET}
-    RPI_KERN_BRANCH=rpi-4.9.y
 
     get_kernel_release() { (cd ${KERNEL_WORK}/linux; ARCH=arm CROSS_COMPILE=${CTARGET}- make kernelrelease;) }
     get_kernel_version() { (cd ${KERNEL_WORK}/linux; ARCH=arm CROSS_COMPILE=${CTARGET}- make kernelversion;) }
