fe6c0c73729df58bbf7fd02abaeb9cb46e591e02
d4g33z <jeff@du.local>
2018-07-25 00:14:31 -0400

    Move config check into sysroot_install

--- sysroot.sh
+++ sysroot.sh
@@ -13,24 +13,25 @@ echo -e $XX
 CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${CWD}/config
 
-for ((i=0; i< ${#INSTALL_VARS[@]}; i++)); do
-        echo -e $BO${INSTALL_VARS[$i]} = ${!INSTALL_VARS[$i]};
-done;
-echo -e $XX
-
-if prompt_input_yN "does this look right"; then
-    echo "Ok, running sysroot_install"
-else
-    echo "Fix it and try again"
-    return 1
-fi
-
 sysroot_install()
 {
     if [ $(id -u) -ne 0 ]; then
         echo "error: must run as root"
         return 1
     fi
+
+    for ((i=0; i< ${#INSTALL_VARS[@]}; i++)); do
+            echo -e $BO${INSTALL_VARS[$i]} = ${!INSTALL_VARS[$i]};
+    done;
+    echo -e $XX
+
+    if prompt_input_yN "does this look right"; then
+        echo "Ok, running sysroot_install"
+    else
+        echo "Fix it and try again"
+        return 1
+    fi
+
 
     ################################################################################
     echo -e $UL$MAG"Install the Stage 3 Tarball"
