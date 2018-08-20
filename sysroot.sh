2735a42d76614c57f9996b0c6c3b28704c011690
d4g33z <jeff@mu.local>
2018-08-20 16:05:34 -0400

    distcc install scriptlet

--- sysroot.sh
+++ sysroot.sh
@@ -485,6 +485,24 @@ sysroot_run_in_chroot()
     sysroot_umount $1 || return 1
 }
 
+sysroot_install_distcc() {
+    cat > /tmp/install_distcc_in_chroot.sh << EOF
+emerge distcc
+cd /usr/lib/distcc/bin
+rm c++ g++ gcc cc
+cat > ${CHOST}-wrapper << EOF2
+#!/bin/bash
+exec /usr/lib/distcc/bin/${CHOST}-g\${0:$[-2]} "\$@"
+EOF2
+ln -s ${CHOST}-wrapper cc
+ln -s ${CHOST}-wrapper gcc
+ln -s ${CHOST}-wrapper g++
+ln -s ${CHOST}-wrapper c++
+
+EOF
+#   sysroot_run_in_chroot $1 /tmp/install_distcc_in_chroot.sh
+
+}
 
 sysroot_mount()
 {
